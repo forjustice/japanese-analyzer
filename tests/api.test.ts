10daf2d55e54bb99a2e7654656b433e447087d0a
forjustice <chintomorrow9@gmail.com>
2025-07-15 04:40:58 +0900

    update

--- tests/api.test.ts
+++
@@ -1,6 +0,0 @@
-import assert from 'assert';
-import { getApiEndpoint } from '../app/services/api';
-
-assert.strictEqual(getApiEndpoint('/analyze'), '/api/analyze');
-assert.strictEqual(getApiEndpoint('/tts'), '/api/tts');
-console.log('All tests passed');
